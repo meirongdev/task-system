934f4d4d46b8beffc12eda14e80871546a8a2f15
Meirong <meirongdev@gmail.com>
2024-08-24 18:42:51 +0800

    feat: use supabase to login

--- src/app/Login.js
+++ src/app/Login.js
@@ -1,15 +1,31 @@
 "use client";
+import { getSupabaseBrowserClient } from "@/supabase-utils/browserClient";
 import Link from "next/link";
 import { useRef } from "react";
+import { useRouter } from "next/navigation";
 
 export const Login = ({ isPasswordLogin }) => {
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
+  const supabase = getSupabaseBrowserClient();
+  const router = useRouter();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (isPasswordLogin) {
+          supabase.auth.signInWithPassword({
+            email: emailInputRef.current.value,
+            password: passwordInputRef.current.value,
+          }).then((response) => {
+            if (response.data?.user) {
+              // 这种方式用户体验不好，因为用户通过历史记录可以访问这个页面
+              // 但是你没有权限，需要重新登录
+              router.push("/tasks");
+            } else {
+              alert("User login failed");
+            }
+          });
           alert("User wants to login with password " + passwordInputRef.current.value);
         } else {
           alert("User wants to login with magic link " + emailInputRef.current.value);
--- src/middleware.js
+++ src/middleware.js
@@ -1,8 +1,23 @@
 import { getSupabaseReqResClient } from "@/supabase-utils/reqResClient";
+import { NextResponse } from "next/server";
 
 export async function middleware(request) {
   const { supabase, response } = getSupabaseReqResClient({ request });
-
+  // getSession() will load and refresh the session if needed
+  // 它会返回session中的数据，但是这些数据只是从cookie中解析出来的，并未加密，容易被篡改
+  // 此处更安全的做法是使用supabase.auth.getUser()，但该方法会有额外的api请求，相对比较慢
+  // 此处能使用getSession()，是因为此处只是做重定向，如果用户不合法，重定向后仍然无法访问受保护的页面
+  const session = await supabase.auth.getSession();
+  const requestedPath = request.nextUrl.pathname;
+  const sessionUser = session.data?.session?.user;
+  if (requestedPath.startsWith("/tasks") && !sessionUser) {
+    // https://nextjs.org/docs/messages/middleware-relative-urls
+    response.value = NextResponse.redirect(new URL("/", request.url));
+  } else if (requestedPath === "/"){
+    if (sessionUser) {
+      response.value = NextResponse.redirect(new URL("/tasks", request.url));
+    }
+  }
   return response.value;
 }
 
