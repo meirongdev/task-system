cf347cfccfc30ba8018fac2e708771a79e2ebdcd
Meirong <meirongdev@gmail.com>
2024-08-24 08:53:40 +0800

    feat: integrate with @supabase/ssr

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,19 @@
+"use client";
 import Image from "next/image";
+import { getSupabaseBrowserClient } from "@/supabase-utils/browserClient";
+import { useEffect } from "react";
+
 
 export default function Home() {
+  useEffect(() =>{
+    const supabase = getSupabaseBrowserClient();
+    supabase.storage.listBuckets().then(
+      (result) => {
+        console.log("Buckets", result);
+      }
+    );
+  }, []);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
